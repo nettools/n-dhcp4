8bd4f990cf0547dabf956a418aff5a409fcd56d5
Tom Gundersen <teg@jklm.no>
2018-09-19 11:12:12 +0100

    packet/sockaddr_ll2: set the size to what is maximally supported by the kernel
    Signed-off-by: Tom Gundersen <teg@jklm.no>

--- src/util/packet.h
+++ src/util/packet.h
@@ -2,6 +2,7 @@
 
 #include <inttypes.h>
 #include <linux/if_packet.h>
+#include <linux/netdevice.h>
 #include <netinet/in.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -19,7 +20,7 @@ struct sockaddr_ll2 {
         unsigned short  sll_hatype;
         unsigned char   sll_pkttype;
         unsigned char   sll_halen;
-        unsigned char   sll_addr[20];
+        unsigned char   sll_addr[MAX_ADDR_LEN];
 };
 
 uint16_t packet_internet_checksum(uint8_t *data, size_t len);
