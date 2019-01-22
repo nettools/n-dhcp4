96cdc237de765f0d39c9f9555ac1fba69d8332d1
David Herrmann <dh.herrmann@gmail.com>
2019-01-22 14:26:06 +0100

    util/socket: enable SO_BINDTOIFINDEX
    This enables the socket_bind_if() helper to make use of the new
    SO_BINDTOIFINDEX fallback. It has been disabled for now, since the
    relevant kernel patches have still been pending.
    
    As of few days ago, the patches are queued in davem/net-next and to be
    merged for linux-5.1.
    
    Signed-off-by: David Herrmann <dh.herrmann@gmail.com>

--- src/util/socket.c
+++ src/util/socket.c
@@ -53,9 +53,9 @@ int socket_SIOCGIFNAME(int socket, int ifindex, char (*ifnamep)[IFNAMSIZ]) {
  *
  * This binds the socket given via @socket to the network interface specified
  * via @ifindex. It uses the underlying SO_BINDTODEVICE ioctl of the linux
- * kernel. However, if available, if prefers the newer SO_BINDTOIF ioctl, which
- * avoids resolving the interface name temporarily, and thus does not suffer
- * from a race-condition.
+ * kernel. However, if available, if prefers the newer SO_BINDTOIFINDEX ioctl,
+ * which avoids resolving the interface name temporarily, and thus does not
+ * suffer from a race-condition.
  *
  * Return: 0 on success, negative error code on failure.
  */
@@ -66,31 +66,41 @@ int socket_bind_if(int socket, int ifindex) {
         assert(ifindex >= 0);
 
         /*
-         * We first try the newer SO_BINDTOIF. If it is not available on the
-         * running kernel, we fall back to SO_BINDTODEVICE. This, however,
+         * We first try the newer SO_BINDTOIFINDEX. If it is not available on
+         * the running kernel, we fall back to SO_BINDTODEVICE. This, however,
          * requires us to first resolve the ifindex to an ifname. Note that
          * this is racy, since the device name might theoretically change
          * asynchronously.
          *
-         * Using 0 as ifindex will remove the device-binding. For SO_BINDTOIF
-         * we simply pass-through the 0 to the kernel, which recognizes this
-         * correctly. For SO_BINDTODEVICE we pass the empty string, which the
-         * kernel recognizes as a request to remove the binding.
+         * Using 0 as ifindex will remove the device-binding. For
+         * SO_BINDTOIFINDEX we simply pass-through the 0 to the kernel, which
+         * recognizes this correctly. For SO_BINDTODEVICE we pass the empty
+         * string, which the kernel recognizes as a request to remove the
+         * binding.
+         *
+         * The commit introducing SO_BINDTOIFINDEX first appeared in linux-5.1:
+         *
+         *     commit f5dd3d0c9638a9d9a02b5964c4ad636f06cf7e2c
+         *     Author: David Herrmann <dh.herrmann@gmail.com>
+         *     Date:   Tue Jan 15 14:42:14 2019 +0100
+         *
+         *         net: introduce SO_BINDTOIFINDEX sockopt
+         *
+         * In older kernels, setsockopt(2) is guaranteed to return ENOPROTOOPT
+         * for this ioctl.
          */
 
-#if 0 /* SO_BINDTOIF is not yet upstream, so disable for now */
-#ifdef SO_BINDTOIF
+#ifdef SO_BINDTOIFINDEX
         r = setsockopt(socket,
                        SOL_SOCKET,
-                       SO_BINDTOIF,
+                       SO_BINDTOIFINDEX,
                        &ifindex,
                        sizeof(ifindex));
         if (r >= 0)
                 return 0;
         else if (errno != ENOPROTOOPT)
                 return -errno;
-#endif /* SO_BINDTOIF */
-#endif
+#endif /* SO_BINDTOIFINDEX */
 
         if (ifindex > 0) {
                 r = socket_SIOCGIFNAME(socket, ifindex, &ifname);
